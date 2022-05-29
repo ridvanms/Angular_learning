b7a0a300cc495808be2c32560ca053ed6b9fe79f
ridvanms <ridvans@abv.bg>
2022-05-29 15:05:16 +0300

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, FormsModule],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
-});
+})
 export class AppModule {}
+// something
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,7 +1,10 @@
-// import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-// @Component({
-//   Selector: "app-server",
-//   template: "",
-// })
-// export class ServerComponent {}
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+}
